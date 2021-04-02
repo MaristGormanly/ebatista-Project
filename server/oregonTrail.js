9bf59ee1ee4eeda947857ab08823c2ef3a8ef6b0
Emmanuel <emmanuel.batista1@marist.edu>
2021-04-02 16:50:09 -0400

    part 3

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.static('client/public'));
 const port = 1337
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root: './client/views'})
 })
 
 app.listen(port, () => {
